88d128057d87e85de510a1610e26f980e4ebc4fd
wpf <2234017537@qq.com>
2023-04-23 17:54:19 +0800

    构建了一个cat类

---
+++ PetEat/src/com/mashibing/Cat.java
@@ -0,0 +1,16 @@
+package com.mashibing;
+
+public class Cat extends Pet{
+    private String eat;
+
+    public String getEat() {
+        return eat;
+    }
+
+    public void setEat(String eat) {
+        this.eat = eat;
+    }
+
+    public Cat() {
+    }
+}
