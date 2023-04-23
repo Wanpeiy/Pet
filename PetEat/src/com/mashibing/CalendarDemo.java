31b79a135cdc3dccbfeebdbfaccea384e240d5f0
wpf <2234017537@qq.com>
2023-04-23 19:45:42 +0800

    构建一个Calendar类

---
+++ PetEat/src/com/mashibing/CalendarDemo.java
@@ -0,0 +1,23 @@
+package com.mashibing;
+
+import java.sql.SQLOutput;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarDemo {
+
+
+    public static void main(String[] args) {
+        Calendar cal=new GregorianCalendar();
+        System.out.println(cal.toString());
+        //获取日期
+        System.out.println(cal.get(Calendar.YEAR));
+        cal.get(Calendar.MONTH);
+        //设置参数
+        cal.set(Calendar.YEAR,1990);
+        cal.set(Calendar.MONTH,3);
+        System.out.println(cal);
+
+
+    }
+}
